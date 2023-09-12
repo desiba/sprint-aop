45a0dfd6e8b8d658c34de7523ea94baa03363a13
desiba <desiba_1@yahoo.com>
2023-09-12 15:41:53 +0100

    initialize project

---
+++ src/main/java/com/desmond/sprintaop/SprintAopApplication.java
@@ -0,0 +1,13 @@
+package com.desmond.sprintaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintAopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desmond/sprintaop/SprintAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desmond.sprintaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
